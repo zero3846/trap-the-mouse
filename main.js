652f5dfe0453fc52820a0cb7d59865f30b8ed819
Miguel Borromeo <iggy47@gmail.com>
2026-09-09 00:58:35 -0500

    Slowed down framerate to 5 fps

--- main.js
+++ main.js
@@ -6,13 +6,34 @@ function main() {
     const canvas = document.querySelector("#main");
     const game = new Game(canvas);
 
-    function drawFrame() {
-        updateGame(game);
-        renderGame(game);
-        requestAnimationFrame(drawFrame);
+    let numFrames = 0;
+    let lastTime = 0
+    const fpsTarget = 5;
+    const renderTimeout_msecs = 1.0 / fpsTarget * 1000;
+    const logFPS = false;
+
+    function animate(currentTime) {
+        const elapsedTime = currentTime - lastTime;
+
+        if (elapsedTime > renderTimeout_msecs) {
+            lastTime = currentTime;
+            numFrames++;
+            updateGame(game);
+            renderGame(game);
+        }
+
+        requestAnimationFrame(animate);
     }
 
-    requestAnimationFrame(drawFrame);
+    requestAnimationFrame(animate);
 
     setupEventListeners(game);
+
+    if (logFPS) {
+        setInterval(() => {
+            const fps = numFrames;
+            console.log(fps + " FPS");
+            numFrames = 0;
+        }, 1000)
+    }
 }
